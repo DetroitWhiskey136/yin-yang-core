a54867c07b999b9fb2027a13b8cbdae497626595
Danno3817 <detroitwhiskey136@gmail.com>
2021-05-19 18:01:39 -0700

    typedoc setup and darker theme setup

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * This is a test to make sure the docs are working correctly (can be ignored for now)
+ * ```JS
+ * const test = test()
+ * ```
+ * @returns
+ */
+function test() {
+    return;
+}
